8a64f8f8634157c42f9659099f18c9d4c708a426
JanTilles <jan.tilles@gmail.com>
2018-02-23 15:39:34 +0200

    Html test

--- wsgi.py
+++ wsgi.py
@@ -9,9 +9,16 @@ def hello():
 
 @application.route("/log")
 def host():
-    return socket.gethostname()+"\n"
-
-host()
+    user = {'username': 'miguel'}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>'''
     
 if __name__ == "__main__":
     application.run()
