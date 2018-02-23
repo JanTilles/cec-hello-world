976512d7b0467cfd3b8f2df62d891c2181a9494b
JanTilles <jan.tilles@gmail.com>
2018-02-23 13:54:19 +0200

    New subpage

--- wsgi.py
+++ wsgi.py
@@ -7,6 +7,10 @@ application = Flask(__name__)
 def hello():
     return socket.gethostname()+"\n"
 
+@application.route("/log")
+def log():
+    return socket.gethostname()+"\n"
+
 
 if __name__ == "__main__":
     application.run()
