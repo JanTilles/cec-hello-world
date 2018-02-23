0b9098301b904e9021ef0d0d5895611bc22cea42
JanTilles <jan.tilles@gmail.com>
2018-02-23 13:32:17 +0200

    Fix
    ...

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 import socket
+import datetime
 from flask import Flask
 
 application = Flask(__name__)
@@ -6,8 +7,14 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     
-    return socket.gethostname()+"\n"
+	return socket.gethostname()+"\n"
 
+@application.route("/log")
+def log():
+    host = socket.gethostname()
+	timestamp = datetime.datetime.now()
+	
+	return host, timestamp"\n"
 
 if __name__ == "__main__":
     application.run()
