cd712be044b2d3b00110c1f266f7c3e7aa24db3f
JanTilles <jan.tilles@gmail.com>
2018-02-23 13:42:50 +0200

    Fix in GitHub

--- wsgi.py
+++ wsgi.py
@@ -6,13 +6,13 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-	return socket.gethostname()+"\n"
+    return socket.gethostname()+"\n"
 
 @application.route("/log/")
 def log():
-	host = socket.gethostname()
-	timestamp = datetime.datetime.now()
-	return host, timestamp"\n"
+    host = socket.gethostname()
+    timestamp = datetime.datetime.now()
+    return host, timestamp"\n"
 
 if __name__ == "__main__":
     application.run()
