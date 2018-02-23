32b4a2cbbb95de14554bc96878958bc86f0cb3d2
JanTilles <jan.tilles@gmail.com>
2018-02-23 15:43:59 +0200

    Test 2.0
    ....

--- wsgi.py
+++ wsgi.py
@@ -16,7 +16,7 @@ def host():
             <title>Home Page - Microblog</title>
         </head>
         <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
+            <h1>Hello , ''' + user['username'] + '''!</h1>
         </body>
     </html>'''
     
