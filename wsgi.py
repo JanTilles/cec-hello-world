3d6546610413ea8d477751db2dd7cc005e2a4ea6
JanTilles <jan.tilles@gmail.com>
2018-02-26 19:05:26 +0200

    Update to code

--- wsgi.py
+++ wsgi.py
@@ -1,33 +1,7 @@
 import socket
-import datetime
-import os.path
 from flask import Flask
 
 application = Flask(__name__)
-
-directory="mnt"
-
-def writelog():
-    host= socket.gethostname()
-    ts= datetime.datetime.now()
-    #if(os.path.exists(directory)):
-    log = open(directory+"/log.txt", "a+")
-    log.write( host+" "+str(ts)+"\n")
-    log.close()
-    return
-
-def readlog():
-    log = open(directory+"/log.txt", "r")
-    content =log.read()
-    log.close()
-    return content
-
-def getlog():
-    writelog()
-    data=readlog()
-    return data
-
-#application = Flask(__name__)
 
 @application.route("/")
 def hello():
@@ -35,6 +9,30 @@ def hello():
 
 @application.route("/log")
 def host():
+    import datetime
+    import os.path
+    directory="mnt"
+
+    def writelog():
+        host= socket.gethostname()
+        ts= datetime.datetime.now()
+        #if(os.path.exists(directory)):
+        log = open(directory+"/log.txt", "a+")
+        log.write( host+" "+str(ts)+"\n")
+        log.close()
+        return
+
+    def readlog():
+        log = open(directory+"/log.txt", "r")
+        content =log.read()
+        log.close()
+        return content
+
+    def getlog():
+        writelog()
+        data=readlog()
+        return data
+    
     return getlog()+"\n"
     
 if __name__ == "__main__":
